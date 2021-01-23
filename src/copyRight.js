3c637e67815f78cb95bc1304945a19684f018dbb
md sajjat hossain <cgsaleh83@gmail.com>
2021-01-23 12:38:11 +0600

    thats is update

---
+++ src/copyRight.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './util.css';
+
+const CopyRight = () => {
+    return (
+        <div className="copyRight__container">
+            <p>Copyright 2021 © All Rights Reserved By <a href="https://itgardenltd.com/" target="_blank" rel="noopener noreferrer">IT Garden Ltd.</a></p>
+        </div>
+    );
+};
+
+export default CopyRight;
