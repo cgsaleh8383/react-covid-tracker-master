58734c81fe442a9c43e011d362f6dece65845b64
md sajjat hossain <cgsaleh83@gmail.com>
2021-01-23 12:40:20 +0600

    thats is update

--- src/copyRight.js
+++ src/copyRight.js
@@ -4,6 +4,7 @@ import './util.css';
 const CopyRight = () => {
     return (
         <div className="copyRight__container">
+           {/* git remote add origin https://github.com/cgsaleh8383/react-covid-tracker-master.git */}
             <p>Copyright 2021 © All Rights Reserved By <a href="https://itgardenltd.com/" target="_blank" rel="noopener noreferrer">IT Garden Ltd.</a></p>
         </div>
     );
